19ce7f560452dcab7eff989e5f8749a39b687b6e
acarmonabelmonte <acarmonabelmonte@gmail.com>
2019-06-02 13:08:22 +0200

    plantilla cargar preguntas

--- api/src/routes/question.routes.js
+++ api/src/routes/question.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-const Question = require('../models/question')
+const Question = require('../models/question');
 
 router.get('/', async (req, res) => {
     const preguntas = await Question.find();
--- static/src/App.js
+++ static/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './Scenes/Login/Login'
+import Login from './Scenes/Login/Login';
+import RandomQuestions from './Scenes/LoadQuestions/RandomQuestions'
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <RandomQuestions/>
+      {/* <Login/> */}
     </div>
   );
 }
