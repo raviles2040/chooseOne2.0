3222295313ed4c45d6dafbe4cd414b1f5de19e1d
Raúl <raul2040@outlook.es>
2019-06-02 13:00:26 +0200

    Componente de crear pregunta creado

--- static/src/App.js
+++ static/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Scenes/Login/Login'
+import QuestionComponent from './Scenes/QuestionComponent/QuestionComponent'
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <QuestionComponent/>
     </div>
   );
 }
