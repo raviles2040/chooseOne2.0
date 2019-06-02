7a17924736dc5a2e64f1be6f8eaebb832909767a
acarmonabelmonte <acarmonabelmonte@gmail.com>
2019-06-02 11:29:53 +0200

    base de la api creada

---
+++ api/src/index.js
@@ -0,0 +1,29 @@
+// Importamos express para facilitarnos crear el servidor y realizar llamadas HTTP
+const express = require('express');
+const path = require('path');
+
+const { mongoose } = require('./database');
+
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(express.json());
+
+// Permisions
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
+});
+
+//Routes
+app.use('/api/questions', require('./routes/question.routes'));
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
---
+++ api/src/routes/question.routes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+const Question = require('../models/question')
+
+router.get('/', async (req, res) => {
+    const preguntas = await Question.find();
+    res.json(preguntas);
+});
+
+router.get('/:id', async (req, res) => {
+    const pregunta = await Question.findById(req.params.id);
+    res.json(pregunta);
+});
+
+router.post('/', async (req, res) => {
+    const { question, optionOne, optionTwo } = req.body;
+    const pregunta = new Question({ question, optionOne, optionTwo });
+    await pregunta.save();
+    res.json({status: 'Question Saved'});
+});
+
+router.put('/:id', async (req, res) => {
+    const { question, optionOne, optionTwo } = req.body;
+    const newQuestion = { question, optionOne, optionTwo };
+    await Question.findByIdAndUpdate(req.params.id, newQuestion);
+    res.json({status: 'Question Updated'});
+});
+
+router.delete('/:id', async (req, res) => {
+    await Question.findByIdAndRemove(req.params.id);
+    res.json({status: 'Question Deleted'});
+});
+
+module.exports = router;
