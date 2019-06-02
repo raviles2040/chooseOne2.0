6c98a1ce5ea6bbb06a5da3729cee8ae4ff8ca58b
Juan Antonio Pujals Echeverría <jpujals@quonext-tur.com>
2019-06-02 16:30:16 +0200

    multer uploading file

--- api/src/database.js
+++ api/src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/chooseOne';
+const URI = 'mongodb+srv://admin:admin123_@cluster1-gc1nl.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(URI, { useNewUrlParser: true })
     .then(db => console.log('DB is connected'))
